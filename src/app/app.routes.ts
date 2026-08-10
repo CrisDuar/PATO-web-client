f9d0e90edbc4b87e65f4edc7747e8e9805475f3c
luky <jaaa736504@gmail.com>
2026-08-10 08:52:17 -0500

    feat: formulario de registro con placeholder de imagen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {Registry} from './pages/registry/registry/registry';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'',component: Registry}
+];
