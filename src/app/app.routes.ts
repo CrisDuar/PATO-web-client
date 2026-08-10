57e25843ac7e56e6274192824fee549e22d38cf9
luky <jaaa736504@gmail.com>
2026-08-10 09:53:25 -0500

    feature: formulario para encontrar cuenta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {Registry} from './pages/registry/registry/registry';
+import {FindAccount} from './pages/account-recovery/account-recovery'
 
 export const routes: Routes = [
-  {path:'',component: Registry}
+  {path:'',component: FindAccount}
 ];
