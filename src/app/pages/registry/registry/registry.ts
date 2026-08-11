75c3cd9c9f7febeb8df19df216af7d411b532d26
luky <jaaa736504@gmail.com>
2026-08-11 11:04:26 -0500

    feat: mensajes de error

--- src/app/pages/registry/registry/registry.ts
+++ src/app/pages/registry/registry/registry.ts
@@ -1,11 +1,13 @@
-import { Component, signal, computed } from '@angular/core';
+import {Component, signal, computed, inject} from '@angular/core';
 import { NavbarPrelogin } from '../../../components/navbar-prelogin/navbar-prelogin';
-import { routes } from '../../../app.routes';
 import { Router } from '@angular/router';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {MatFormField, MatLabel} from '@angular/material/input';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-registry',
-  imports: [NavbarPrelogin],
+  imports: [NavbarPrelogin, MatLabel, MatFormField, MatIcon],
   templateUrl: './registry.html',
   styleUrl: './registry.css',
 })
@@ -15,20 +17,43 @@ export class Registry {
   emailCampo = signal('');
   passwordCampo = signal('');
   passwordConfirmacionCampo = signal('');
-
+  private _snackBar = inject(MatSnackBar)
+  abrirMensaje(Mensaje: string) {
+    this._snackBar.open(Mensaje, 'Cerrar', {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'bottom',
+      panelClass: ['error-snackbar']
+    });
+  }
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
   passwordsNoCoinciden = computed(() => {
     const pass = this.passwordCampo();
     const confirm = this.passwordConfirmacionCampo();
+
     return confirm !== '' && pass !== confirm;
   });
   passwordsNoRequisitos = computed(() => {
     const pass = this.passwordCampo();
+
     return pass !== '' && !this.PASSWORD_REGEX.test(pass);
   });
   registrarCuenta() {
     const email = this.emailCampo();
     const pass = this.passwordCampo();
     const confirm = this.passwordConfirmacionCampo();
+    if(this.passwordsNoCoinciden()){
+      this.abrirMensaje("Contraseñas no coinciden")
+      return;
+    }
+    if(this.passwordsNoRequisitos()){
+      this.abrirMensaje("Contraseña no cumple con los requisitos minimos")
+      return;
+    }
     /*
     Se hace una verificacion con los metodos ya existentes para verificación de campos si coincides y si cumplen los requisitos:
     - Al menos una minuscula
@@ -42,6 +67,5 @@ export class Registry {
     Si si existe: mostrar mensaje de error explicitamente diciendo que ya hay una cuenta vinculada a ese correo
      */
     console.log('Registrando...');
-    this.router.navigate(['/']);
   }
 }
