5db75915349306ea84598d50c9cb51c2c9c97e0a
Luky <jaaa736504@gmail.com>
2026-08-17 11:28:57 -0500

    feat/fix: feats: [(Inclusión de navbar para chatbot, prediction-viewer, map-viewer), (Redirección de login a pagina map-viewer), (Redirección a login post-click de cierre de sesión)], Fixes [(Navbar con clases de activación para dinamismo de esta, ruta principal sera Login)]

--- src/app/pages/chat-bot/chat-bot.ts
+++ src/app/pages/chat-bot/chat-bot.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Navbar } from '../../components/navbar/navbar';
 
 @Component({
   selector: 'app-chat-bot',
-  imports: [],
+  imports: [Navbar],
   templateUrl: './chat-bot.html',
   styleUrl: './chat-bot.css',
 })
