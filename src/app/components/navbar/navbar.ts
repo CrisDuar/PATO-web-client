89e61fdfe1fb34917a3fe8b21ca2d9144bddf3d9
Dalyhae <ilovethebabyducklings@gmail.com>
2026-08-17 17:53:58 -0500

    feat: Menu update Edit profile

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, model } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import { RouterLink} from "@angular/router";
 import { RouterModule } from '@angular/router';
 import {MatMenuModule} from '@angular/material/menu';
+import { EditProfile } from '../edit-profile/edit-profile';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatButtonModule, MatToolbarModule, MatIconModule, RouterLink, RouterModule, MatMenuModule],
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule, RouterLink, RouterModule, MatMenuModule, EditProfile],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  readonly animal = signal(' ');
+  readonly name = model(' ');
+  readonly editProfile = inject(MatDialog);
+
+  // Abrir el modal de editar perfil
+  openEditProfile(): void {
+    const dialogRef = this.editProfile.open(EditProfile, {
+      width: '420px',
+      data: {name: this.name(), animal: this.animal()},
+  });
+
+  }
+}
