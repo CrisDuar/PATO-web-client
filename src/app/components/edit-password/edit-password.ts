a4852af2e51fd01e78dfa8822121290ec3de9de0
Dalyhae <ilovethebabyducklings@gmail.com>
2026-08-18 03:03:27 -0500

    refactor: Windows for editing name, email, and password

--- src/app/components/edit-password/edit-password.ts
+++ src/app/components/edit-password/edit-password.ts
@@ -1,11 +1,21 @@
-import { Component} from '@angular/core';
-import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { Component, inject, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
-import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { EditProfile } from '../edit-profile/edit-profile';
+import {
+  MatDialog,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { merge } from 'rxjs';
 
 
 @Component({
@@ -18,10 +28,68 @@ import { MatDialog } from '@angular/material/dialog';
     MatButtonModule,
     MatListModule,
     MatIconModule,
+
   ],
   templateUrl: './edit-password.html',
   styleUrl: './edit-password.css',
 })
 export class EditPassword {
+  private snackBar = inject(MatSnackBar);
+  private dialogRef = inject(MatDialogRef<EditProfile>);
+  errorMessage = signal('');
+
+  readonly currentPassword = new FormControl('', [Validators.required]);
+
+  readonly newPassword = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8),
+    Validators.pattern(/(?=.*[A-Z])(?=.*[0-9])/),
+  ]);
+
+  readonly confirmPassword = new FormControl('', [Validators.required]);
+
+  constructor() {
+    merge(this.newPassword.statusChanges, this.newPassword.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+  }
+    
+  onSave(): void {
+    // Si ninguno es válido
+    if (this.currentPassword.invalid || this.newPassword.invalid || this.confirmPassword.invalid) {
+      this.updateErrorMessage();
+      return;
+    }
+
+    
+    if (this.newPassword.value !== this.confirmPassword.value) {
+      this.snackBar.open('Las contraseñas no coinciden', undefined, {
+        duration: 3000,
+        verticalPosition: 'top',
+      });
+      return;
+    }
+
+    // Bocadillo de contraseña actualizada con éxito
+    this.snackBar.open('¡Contraseña actualizada correctamente!', 'Cerrar', {
+      duration: 3000,
+      verticalPosition: 'bottom',
+    });
+
+    this.dialogRef.close(true);
+  }
+
+  // Mensajes de error
+  updateErrorMessage(): void {
+    if (this.newPassword.hasError('required')) {
+      this.errorMessage.set('Es obligatorio llenar el campo');
+    } else if (this.newPassword.hasError('minlength')) {
+      this.errorMessage.set('Mínimo 8 caracteres');
+    } else if (this.newPassword.hasError('pattern')) {
+      this.errorMessage.set('Debe incluir al menos una mayúscula y un número');
+    } else {
+      this.errorMessage.set('');
+    }
+  }
 
 }
