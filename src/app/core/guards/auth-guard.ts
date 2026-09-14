e20e13a4b5800d0bb020615ef81abd139194f6ae
Dalyhae <ilovethebabyducklings@gmail.com>
2026-09-14 01:31:54 -0500

    fix: routes

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -9,6 +9,7 @@ export const authGuard: CanActivateFn = (route, state) => {
   if(authService.isAuthenticated()){
     return true;
   }else {
-    return router.navigate(['/login']);
+    router.navigate(['/login']);
+    return false;
   }
 };
