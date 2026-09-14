e02f22e67b1cea4fcb4a6a4cab1e329edd14b7bf
Dalyhae <ilovethebabyducklings@gmail.com>
2026-09-14 01:47:13 -0500

    fix: routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,31 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '/login',
+    path: 'login',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'register',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'accountRecovery',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'col',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'col-dept',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'latin',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: '**',
     renderMode: RenderMode.Prerender
   }
 ];
